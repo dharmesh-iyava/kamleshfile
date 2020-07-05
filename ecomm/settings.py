766c0e5288965c14de57cf535c4a797011ce7cef
dharmesh-iyava <60869379+dharmesh-iyava@users.noreply.github.com>
2020-07-05 20:10:16 +0530

    Update settings.py

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mwxv1(a3kh1*3y39ou($8e7mmewkm0#twzgt1h6$a0(vnw0*he'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['amazingsoppy.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['djangoimagefield.herokuapp.com','127.0.0.1']
 
 
 # Application definition
